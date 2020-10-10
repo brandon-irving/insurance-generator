f9fa32a718b40257c6546daae712c33e161b8d76
birving2 <brandon@peakactivity.com>
2020-10-10 04:49:12 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ContextStateProvider } from 'dynamic-context-provider'
 import './App.css';
+import GeicoTemplate from './Components/GeicoTemplate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <ContextStateProvider stateConfig={{}}>
+        <GeicoTemplate />
+      </ContextStateProvider>
     </div>
   );
 }
---
+++ src/Components/GeicoTemplate.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import geicoImage from '../static/background1.jpg';
+
+const GeicoTemplate = () => {
+    const name = 'Brandon Jamal Irving'
+    const addressLine1 = '618 NW 13TH ST APT 27'
+    const addressLine2 = 'BOCA RATON FL  33486'
+    const year = 2017
+    const make = 'FORD'
+    const model = 'MUSTANG'
+    return (
+        <div>
+            <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+            <style type="text/css" dangerouslySetInnerHTML={{ __html: "\n<!--\nspan.cls_004{font-family:Arial,serif;font-size:8.0px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}\ndiv.cls_004{font-family:Arial,serif;font-size:8.0px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}\nspan.cls_002{font-family:Arial,serif;font-size:11.0px;color:rgb(0,0,0);font-weight:bold;font-style:italic;text-decoration: none}\ndiv.cls_002{font-family:Arial,serif;font-size:11.0px;color:rgb(0,0,0);font-weight:bold;font-style:italic;text-decoration: none}\nspan.cls_005{font-family:Arial,serif;font-size:10.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}\ndiv.cls_005{font-family:Arial,serif;font-size:10.1px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}\nspan.cls_006{font-family:Arial,serif;font-size:6.0px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}\ndiv.cls_006{font-family:Arial,serif;font-size:6.0px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}\nspan.cls_007{font-family:Arial,serif;font-size:8.0px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}\ndiv.cls_007{font-family:Arial,serif;font-size:8.0px;color:rgb(0,0,0);font-weight:normal;font-style:normal;text-decoration: none}\nspan.cls_008{font-family:Arial,serif;font-size:10.1px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}\ndiv.cls_008{font-family:Arial,serif;font-size:10.1px;color:rgb(0,0,0);font-weight:bold;font-style:normal;text-decoration: none}\nspan.cls_010{font-family:Times,serif;font-size:48.0px;color:rgb(169,169,169);font-weight:normal;font-style:normal;text-decoration: none}\ndiv.cls_010{font-family:Times,serif;font-size:48.0px;color:rgb(169,169,169);font-weight:normal;font-style:normal;text-decoration: none}\n-->\n" }} />
+            <div style={{ position: 'absolute', left: '50%', marginLeft: '-306px', top: '0px', width: '612px', height: '792px', borderStyle: 'outset', overflow: 'hidden' }}>
+                <div style={{ position: 'absolute', left: '0px', top: '0px' }}>
+                    <img
+                        src={geicoImage}
+                        width={612} height={792} />                  </div>
+                <div style={{ position: 'absolute', left: '74.88px', top: '7.84px' }} className="cls_004"><span className="cls_004">FLORIDA AUTOMOBILE INSURANCE</span></div>
+                <div style={{ position: 'absolute', left: '371.52px', top: '4.12px' }} className="cls_002"><span className="cls_002">Important Information</span></div>
+                <div style={{ position: 'absolute', left: '74.88px', top: '17.20px' }} className="cls_004"><span className="cls_004">IDENTIFICATION CARD</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '20.48px' }} className="cls_005"><span className="cls_005">Here are your Policy Identification Cards.  Two cards have been</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '28.32px' }} className="cls_006"><span className="cls_006">GEICO GENERAL INSURANCE COMPANY</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '32.00px' }} className="cls_005"><span className="cls_005">provided for each vehicle insured.  Please destroy your old cards</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '39.04px' }} className="cls_004"><span className="cls_004">Policy Number/Florida Code No.   Effective Date</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '43.52px' }} className="cls_005"><span className="cls_005">when the new cards become effective.</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '49.60px' }} className="cls_007"><span className="cls_007">6020-86-08-69/01288</span></div>
+                <div style={{ position: 'absolute', left: '138.72px', top: '49.60px' }} className="cls_007"><span className="cls_007">05-12-20</span></div>
+                <div style={{ position: 'absolute', left: '8.64px', top: '62.16px' }} className="cls_006"><span className="cls_006">[X]PERSONAL INJURY PROTECTION BENEFITS/PROPERTY DAMAGE LIABILITY</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '59.60px' }} className="cls_005"><span className="cls_005">Due to space limitations on the ID card, only the Named Insured</span></div>
+                <div style={{ position: 'absolute', left: '8.40px', top: '71.04px' }} className="cls_006"><span className="cls_006">[X]BODILY INJURY LIABILITY</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '71.12px' }} className="cls_005"><span className="cls_005">and the Co-insured are listed. For a full list of drivers covered</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '81.28px' }} className="cls_004"><span className="cls_004">Named Insured:</span><span className="cls_007"> {name}</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '82.64px' }} className="cls_005"><span className="cls_005">under this policy, please log onto geico.com or reference the</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '94.16px' }} className="cls_005"><span className="cls_005">Drivers section of your Declarations Page, which is included with</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '100.00px' }} className="cls_004"><span className="cls_004">Year</span></div>
+                <div style={{ position: 'absolute', left: '45.12px', top: '100.00px' }} className="cls_004"><span className="cls_004">Make</span></div>
+                <div style={{ position: 'absolute', left: '104.40px', top: '100.00px' }} className="cls_004"><span className="cls_004">Model</span></div>
+                <div style={{ position: 'absolute', left: '170.40px', top: '100.00px' }} className="cls_004"><span className="cls_004">Vehicle ID No.</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '108.88px' }} className="cls_007"><span className="cls_007">{year}</span></div>
+                <div style={{ position: 'absolute', left: '44.40px', top: '108.88px' }} className="cls_007"><span className="cls_007">{make}</span></div>
+                <div style={{ position: 'absolute', left: '95.76px', top: '108.88px' }} className="cls_007"><span className="cls_007">{model}</span></div>
+                <div style={{ position: 'absolute', left: '159.36px', top: '108.88px' }} className="cls_007"><span className="cls_007">1FA6P8TH8H5307376</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '105.68px' }} className="cls_005"><span className="cls_005">your insurance packet.</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '121.76px' }} className="cls_005"><span className="cls_005">Please notify us promptly of any change in your address to be</span></div>
+                <div style={{ position: 'absolute', left: '65.52px', top: '128.80px' }} className="cls_007"><span className="cls_007">Phone Number:</span><span className="cls_004">1-800-841-3000</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '133.28px' }} className="cls_005"><span className="cls_005">sure you receive all important policy documents.  Prompt</span></div>
+                <div style={{ position: 'absolute', left: '39.12px', top: '147.52px' }} className="cls_007"><span className="cls_007">Not valid more than one year from effective date.</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '144.80px' }} className="cls_005"><span className="cls_005">notification will enable us to service you better.</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '160.88px' }} className="cls_005"><span className="cls_005">Your policy is recorded under the name and policy number shown</span></div>
+                <div style={{ position: 'absolute', left: '77.04px', top: '170.08px' }} className="cls_004"><span className="cls_004">FLORIDA AUTOMOBILE  INSURANCE</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '172.40px' }} className="cls_005"><span className="cls_005">on the card.</span></div>
+                <div style={{ position: 'absolute', left: '77.04px', top: '179.44px' }} className="cls_004"><span className="cls_004">IDENTIFICATION CARD</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '189.84px' }} className="cls_006"><span className="cls_006">GEICO GENERAL INSURANCE COMPANY</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '195.44px' }} className="cls_005"><span className="cls_005">If you would like additional ID cards, you can go online to</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '200.56px' }} className="cls_004"><span className="cls_004">Policy Number/Florida Code No.   Effective Date</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '211.12px' }} className="cls_007"><span className="cls_007">6020-86-08-69/01288</span></div>
+                <div style={{ position: 'absolute', left: '139.20px', top: '211.12px' }} className="cls_007"><span className="cls_007">05-12-20</span></div>
+                <div style={{ position: 'absolute', left: '281.52px', top: '207.20px' }} className="cls_008"><span className="cls_008">geico.com</span><span className="cls_005"> or call us at </span><span className="cls_008">1-800-841-3000.</span></div>
+                <div style={{ position: 'absolute', left: '9.12px', top: '222.72px' }} className="cls_006"><span className="cls_006">[X]PERSONAL INJURY PROTECTION BENEFITS/PROPERTY DAMAGE LIABILITY</span></div>
+                <div style={{ position: 'absolute', left: '8.40px', top: '231.60px' }} className="cls_006"><span className="cls_006">[X]BODILY INJURY LIABILITY</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '240.40px' }} className="cls_004"><span className="cls_004">Named Insured:</span><span className="cls_007"> {name}</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '260.56px' }} className="cls_004"><span className="cls_004">Year</span></div>
+                <div style={{ position: 'absolute', left: '45.36px', top: '260.56px' }} className="cls_004"><span className="cls_004">Make</span></div>
+                <div style={{ position: 'absolute', left: '102.96px', top: '260.56px' }} className="cls_004"><span className="cls_004">Model</span></div>
+                <div style={{ position: 'absolute', left: '170.16px', top: '260.56px' }} className="cls_004"><span className="cls_004">Vehicle ID No.</span></div>
+                <div style={{ position: 'absolute', left: '7.20px', top: '268.48px' }} className="cls_007"><span className="cls_007">{year}</span></div>
+                <div style={{ position: 'absolute', left: '43.92px', top: '268.48px' }} className="cls_007"><span className="cls_007">{make}</span></div>
+                <div style={{ position: 'absolute', left: '95.52px', top: '268.48px' }} className="cls_007"><span className="cls_007">{model}</span></div>
+                <div style={{ position: 'absolute', left: '158.88px', top: '268.48px' }} className="cls_007"><span className="cls_007">1FA6P8TH8H5307376</span></div>
+                <div style={{ position: 'absolute', left: '330.24px', top: '274.88px' }} className="cls_005"><span className="cls_005">{name}</span></div>
+                <div style={{ position: 'absolute', left: '65.52px', top: '288.16px' }} className="cls_007"><span className="cls_007">Phone Number:</span><span className="cls_004">1-800-841-3000</span></div>
+                <div style={{ position: 'absolute', left: '330.24px', top: '286.40px' }} className="cls_005"><span className="cls_005">{addressLine1}</span></div>
+                <div style={{ position: 'absolute', left: '330.24px', top: '297.92px' }} className="cls_005"><span className="cls_005">{addressLine2}</span></div>
+                <div style={{ position: 'absolute', left: '39.12px', top: '306.88px' }} className="cls_007"><span className="cls_007">Not valid more than one year from effective date.</span></div>
+                <div style={{ position: 'absolute', left: '156.00px', top: '514.56px' }} className="cls_010"><span className="cls_010">VOID</span></div>
+                <div style={{ position: 'absolute', left: '411.60px', top: '514.56px' }} className="cls_010"><span className="cls_010">VOID</span></div>
+                <div style={{ position: 'absolute', left: '156.00px', top: '667.44px' }} className="cls_010"><span className="cls_010">VOID</span></div>
+                <div style={{ position: 'absolute', left: '411.60px', top: '667.44px' }} className="cls_010"><span className="cls_010">VOID</span></div>
+            </div>
+        </div>
+    );
+
+}
+
+export default GeicoTemplate
