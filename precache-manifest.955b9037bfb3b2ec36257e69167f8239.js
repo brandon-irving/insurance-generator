de45ad6394d161fdaa74e30117bd8f8eb8195379
birving2 <brandon@peakactivity.com>
2021-01-29 18:59:46 -0500

    Updates

---
+++ precache-manifest.955b9037bfb3b2ec36257e69167f8239.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745219e1b0092eb82db0b283953b2bc9",
+    "url": "/insurance-generator/index.html"
+  },
+  {
+    "revision": "15b20e3fb66df723f38e",
+    "url": "/insurance-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f35594c97b6159537b98",
+    "url": "/insurance-generator/static/js/2.7396d289.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/insurance-generator/static/js/2.7396d289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b20e3fb66df723f38e",
+    "url": "/insurance-generator/static/js/main.eb6b6b21.chunk.js"
+  },
+  {
+    "revision": "bcfccb9ae2a71bdb9099",
+    "url": "/insurance-generator/static/js/runtime-main.b1a0f8e9.js"
+  },
+  {
+    "revision": "0286d2ef219b239405d1f0268c328094",
+    "url": "/insurance-generator/static/media/progressiveBackground.0286d2ef.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be02f6a5fb8df2669148a06088995630.js"
+  "/insurance-generator/precache-manifest.955b9037bfb3b2ec36257e69167f8239.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/insurance-generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
